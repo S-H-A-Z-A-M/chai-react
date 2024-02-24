e830f69d369510417a7b4f9a9357e6d1020d5bb9
gaurav <gaurav.gangde21@vit.edu>
2024-02-25 01:32:28 +0530

    react router crash course learned

---
+++ 07reactRouter/src/components/index.js
@@ -0,0 +1,9 @@
+import About from "./About/About.jsx";
+import Footer from "./Footer/Footer.jsx";
+import Headers from "./Header/Header.jsx";
+import Home from "./Home/Home.jsx";
+import Contact from "./Contact/Contact.jsx";
+import User from "./User/User.jsx";
+import Github from "./Github/Github.jsx";
+
+export {About, Footer, Headers, Contact, Home, User, Github};
